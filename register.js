1d40536d4e41eae1bbcc3cd5b176f69bedef566e
Maikon Weber <m.carvalho@grouplinknetwork.com>
2022-10-11 21:39:41 -0300

    Add register and calls

--- register.js
+++ register.js
@@ -5,28 +5,75 @@ const crypto = require('crypto');
 
 
 var client = new Pool({
-            user: "binance",
-            password: "binance",
-            database: "binance",
-            port: 5832,
-            host: "localhost",
-            ssl: false
+   user: "binance",
+   password: "binance",
+   database: "binance",
+   port: 5832,
+   host: "localhost",
+   ssl: false
 });
 
 const query = `
-   INSERT INTO sportRegister ()
+   INSERT INTO spotRegister(
+      position, valor, quantity, date 
+   ) 
+   VALUES (
+      $1, $2, $3, $4
+   );
+   `
+
+const futureQuery = `
+INSERT INTO spotRegister(
+   position, valor, quantity, date 
+) 
+VALUES (
+   $1, $2, $3, $4
+);
+
 `
 
+async function insertRegisterSpot(position, valor, quantity, id) {
+   return await client.query(query, [[position, valor, quantity]])
+}
 
+async function insertRegisterFuture(position, valor, quantity, id) {
+   return await client.query(query, [position, valor, quantity, id])
+}
+
+async function callFuture(position, valor, quantity, id) {
+   const  objectDecisionFuture = {
+     "Up" : async () => {
+
+     },
+     "Down" : async () => {
+
+     }
+   } 
+   return 
+} 
+
+
+async function callSpot(position, valor, quantity, id) {
+   const  objectDecision = {
+     "Up" : async () => {
+
+     },
+     "Down" : async () => {
+
+     }
+   } 
+   return 
+}
 
 
 const objectDecision = {
-            "Spot": async () =>  {
-            
-            },
-
-            "Future": async ()  => {
-            }
-
+   "Spot": async (position, valor, quantity, id) => {
+      await insertRegisterSpot(position, valor, quantity, id)
+   },
+   "Future": async (position, valor, quantity, id) => {
+      await insertRegisterFuture(position, valor, quantity, id)
+   }
 }
+
+
 
